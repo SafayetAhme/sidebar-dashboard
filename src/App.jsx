64b81f7ae4dce640024d20360e59dcdda89b675c
Safayet Ahmed <asafayet21@gmail.com>
2024-02-10 00:29:33 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+// import SideBar from "./components/Sidebar/SideBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import Dashboard from "./pages/Dashboard";
+// import Users from "./pages/Users";
+// import Messages from "./pages/Messages";
+// import FileManager from "./pages/FileManager";
+// import Analytics from "./pages/Analytics";
+// import Order from "./pages/Order";
+// import Saved from "./pages/Saved";
+// import Setting from "./pages/Setting";
+import SideBar from "./component/Sidebar";
+import Dashboard from "./component/page/Dashboard";
+import Users from "./component/page/Users";
+import Messages from "./component/page/Messages";
+import Analytics from "./component/page/Analytics";
+import FileManager from "./component/page/FileManager";
+import Order from "./component/page/Order";
+import Saved from "./component/page/Saved";
+import Setting from "./component/page/Setting";
+
+function App() {
+
+  return (
+    <Router>
+      <SideBar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/file-manager" element={<FileManager />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Setting />} />
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+      </SideBar>
+    </Router>
+  )
+}
+
+export default App
